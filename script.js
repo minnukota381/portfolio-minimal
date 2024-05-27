09addb898b78ecb79a283bb1ed93494c4ebcaffd
Minnu <110650599+minnukota381@users.noreply.github.com>
2024-05-28 02:18:30 +0530

    IC | Portfolio

---
+++ script.js
@@ -0,0 +1,37 @@
+let scrollBtn = document.querySelector(".scroll-button a");
+console.log(scrollBtn);
+let val;
+window.onscroll = function() {
+  if (document.documentElement.scrollTop > 20) {
+    if (scrollBtn) { 
+      scrollBtn.style.display = "block";
+    }
+  } else {
+    if (scrollBtn) { 
+      scrollBtn.style.display = "none";
+    }
+  }
+}
+
+$(document).ready(function(){
+  $('.navbar-toggler').click(function(e){
+    e.stopPropagation();
+    $('#navbarNav').collapse('toggle');
+  });
+
+  $(document).click(function(event){
+    if (!$(event.target).closest('#navbarNav').length && $('#navbarNav').hasClass('show')) {
+      $('#navbarNav').collapse('hide');
+    }
+  });
+});
+
+
+let options = {
+  strings: ["Full Stack Developer", "Web Developer", "Software Developer"],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true,
+  cursorChar: "|",
+};
+let typed = new Typed("#typed-text span", options);
